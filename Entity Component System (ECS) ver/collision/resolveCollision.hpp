668a2b5e6792234bc55b559f6037ed5fcaddd6e8
Mushupwastaken <nolanash20082020@gmail.com>
2025-12-13 20:57:39 -0330

    Add files via upload

---
+++ Entity Component System (ECS) ver/collision/resolveCollision.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Components.hpp"
+
+#include <SFML/System/Vector2.hpp>
+#include <optional>
+
+namespace ne {
+
+std::optional<sf::Vector2f> resolveCollision(ColliderComponent collider_a, TransformableComponent transformable_a, ColliderComponent collider_b, TransformableComponent transformable_b);
+
+} //namespace ne
+
+
